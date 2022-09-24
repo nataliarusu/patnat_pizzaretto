903c2f8cb8abdbb4439ff6499d05ee685869c20a
Natalia Rusu <bonapartik@gmail.com>
2022-09-24 09:40:39 +0100

    add data to the pizza section
    Relates #7

--- scripts/pizza_data.js
+++ scripts/pizza_data.js
@@ -18,4 +18,38 @@ export const p_data=[{
         '15"':19.99
     },
     id:'2'
-}]
+},
+{
+    name:'CAPRICOSA',
+    descr:'Ham, Black Olives, Smoky Sausage & Spinach',
+    img:'../imgs/Capricosa.png',
+    prices:{
+        '9"': 7.99,
+        '12"':10.99,
+        '15"':19.99
+    },
+    id:'3'
+},
+{
+    name:'MEAT LOVERS',
+    descr:'Ham, Meatball, Smoky Sausage, Pepperoni & Spicy Beef',
+    img:'../imgs/MeatLovers.png',
+    prices:{
+        '9"': 8.99,
+        '12"':10.99,
+        '15"':19.99
+    },
+    id:'4'
+},
+{
+    name:'PEPPERONI DELUXE',
+    descr:'Double Pepperoni & Cheese',
+    img:'../imgs/PepperoniDeluxe.png',
+    prices:{
+        '9"': 7.99,
+        '12"':9.99,
+        '15"':18.99
+    },
+    id:'4'
+}
+]
