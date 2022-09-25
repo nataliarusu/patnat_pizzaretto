37f540daeaa5564885d0b601a833f664bcb5e9d5
Natalia Rusu <bonapartik@gmail.com>
2022-09-25 15:28:29 +0100

    add data to vegan pizza section
    Relates #12

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import {renderPizzas} from './scripts/Pizzas.js';
-renderPizzas();
+import {p_data, vegan_p_data} from './scripts/pizza_data.js';
+
+const pizzasSection=document.querySelector('#pizzas');
+const vegan_pizzasSection=document.querySelector('#vegan-pizzas');
+renderPizzas(p_data, pizzasSection);
+renderPizzas(vegan_p_data, vegan_pizzasSection);
